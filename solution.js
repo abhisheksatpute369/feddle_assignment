c3dbbbef399674e845785087eaba9c46308f9c58
abhisheksatpute369 <abhisheksatpute99@gmail.com>
2022-06-29 13:41:43 +0530

    add answer

---
+++ solution.js
@@ -0,0 +1,100 @@
+/*
+You have been given a list of products which is having property productName, quantity and description.
+
+
+PROBLEM STATEMENTS:
+
+1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+{
+  "TV": 2,
+  "AC": 2,
+  "FAN": 1
+}
+
+
+
+2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+[{
+    productName: "TV",
+    quantity: 20,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 10,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+     description: "Ceiling Fan"
+  }
+]
+
+*/
+
+
+
+
+const listOfProducts = [{
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+    description: "Ceiling Fan"
+  }
+];
+
+/* question one solution */
+
+function getUniqueProductCount(){
+	var product = {};
+  for(var i=0; i<listOfProducts.length; i++)
+  {
+  	if(!product[listOfProducts[i].productName])
+    {
+    	product[listOfProducts[i].productName] = 1;
+    }
+    else
+    {
+    	product[listOfProducts[i].productName]++;
+    }
+  }
+ 	console.log(product)
+}
+getUniqueProductCount();
+
+/* question two solution */
+
+function getUniquePrducts(){
+	const key = 'productName';
+	const uniqueproducts = [...new Map(listOfProducts.map(item =>
+  [item[key], item])).values()];
+	console.log(uniqueproducts);
+
+}
+getUniquePrducts();
